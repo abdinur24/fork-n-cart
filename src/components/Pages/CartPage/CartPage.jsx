fc09c8cf434fc48a21984f64ab577acd35362f72
Abdishakur Nur <abdinur20000@gmail.com>
2022-06-29 11:20:31 -0500

    Revert "Cart add total recipes but doesnt add individual recipe"
    This reverts commit 0b750834f739bc00d36ac6337c2b987f1fbe4e30.
    
    revert

--- src/components/Pages/CartPage/CartPage.jsx
+++ src/components/Pages/CartPage/CartPage.jsx
@@ -7,63 +7,24 @@ function CartPage() {
     const cart = useSelector(store => store.cart)
     const store = useSelector(store => store.recipe)
     console.log('This is in cart', cart)
-    let cartTotal = 0;
     let total = 0;
     
-
-    // if (cart.length < 0) {
-    //     <h3>Add recipe to cart for price</h3>
-    // }
+    if(cart.length < 0){
+        <h3>Add recipe to cart for price</h3>
+    }
 
     return (
         <div>
             <h1>Pricing</h1>
-            {cart.map(item => {
-                return (
-                    <div>
-                        <h3>{item.name}</h3>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Ingredient</th>
-                                    <th>Amount</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            {item.recipe_ingredients.map(ingredient => {
-                                {
-                                    let recipeIngredientPrice = 0
-                                    let ingredientPrice = 0;
-                                    ingredientPrice += ingredient.price/ingredient.amount
-                                    console.log('Price pre gram of ingredient',Number(ingredientPrice.toFixed(3)));
-                                    recipeIngredientPrice += Number(ingredientPrice.toFixed(3))*Number(ingredient.recipe_amount)
-                                    console.log('Price for recipe amount of ingredient',recipeIngredientPrice)
-                                    if(ingredient.recipe_id === item.id){
-                                        total += recipeIngredientPrice;
-                                    }
-                               
-                                    return (
-                                        <>
-                                            <tbody>
-                                                <tr>
-                                                    <td>{ingredient.name}</td>
-                                                    <td>{ingredient.recipe_amount} g</td>
-                                                    <td>{recipeIngredientPrice.toFixed(2)}</td>
-                                                </tr>
-                                            </tbody>
-                                            {/* <h4>Total is:{total.toFixed(2)}</h4> */}
-                                        </>
-                                    )
-                                }
-                            })}
-                            {/* <h3>Total is:{cartTotal.toFixed(2)}</h3> */}
-                        </table>
-                    </div>
-                )
+            {(cart.length < 0) ?
+           <h3>Add recipe to cart for price</h3> :
+           ''
+            }
+            {cart.map(recipe =>{
+              return(
+                  <h1>{recipe.name}</h1>
+              )
             })}
-
-            <h4>Total is: ${total.toFixed(2)}</h4>
-
         </div>
     )
 }
